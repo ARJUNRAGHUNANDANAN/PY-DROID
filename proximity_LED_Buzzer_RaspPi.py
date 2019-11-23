82e3bb5ecad5308e3f22d761abac5da435f228ca
ARJUN RAGHUNANDANAN <arjunregunandanan@gmail.com>
2019-11-23 23:34:48 +0530

    Update proximity_LED_Buzzer_RaspPi.py

--- proximity_LED_Buzzer_RaspPi.py
+++ proximity_LED_Buzzer_RaspPi.py
@@ -1,29 +1,29 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gp
 import time
-GPIO.setmode(GPIO.BCM)
-TRIG=23
-ECHO=24
-BUZZ=25
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(BUZZ,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
-GPIO.output(TRIG,False)
+gp.setmode(gp.BCM)
+trigger=23
+echo=24
+buzzer=25
+gp.setup(trigger,gp.OUT)
+gp.setup(buzzer,gp.OUT)
+gp.setup(echo,gp.IN)
+gp.output(trigger,False)
 print "waiting for the sensor to settle"
 time.sleep(2)
 
 while(True):
-    GPIO.output(TRIG,True)
+    gp.output(trigger,True)
     time.sleep(0.00001)
-    GPIO.output(TRIG,False)
-    while GPIO.input(ECHO)==0:
+    gp.output(trigger,False)
+    while gp.input(echo)==0:
         pulse_start =time.time()
-    while GPIO.input(ECHO)==1:
+    while gp.input(trigger)==1:
         pulse_end =time.time()
     pulse_duration=pulse_end-pulse_start
     distance = pulse_duration*17150
     distance =round(distance,2)
     print "Distance :",distance,"cm"
     if (distance<=15):
-        GPIO.output(BUZZ,True)
+        gp.output(buzzer,True)
     else:
-        GPIO.output(BUZZ,False)
+        gp.output(buzzer,False)
